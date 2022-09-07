28c3ebed21404a35eb9e632679c62a478f244c17
Zhang <645311501@qq.com>
2022-09-07 16:46:52 +0800

    fix bug

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,11 @@
-package example;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @Title: Application
- * @Package: example
+ * @Package: com.example
  * @Description:
  * @Author: zhangp
  * @Date: 2022/9/7 - 15:11
--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,4 +1,4 @@
-package example.controller;
+package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Title: HelloController
- * @Package: example.controller
+ * @Package: com.example.controller
  * @Description:
  * @Author: zhangp
  * @Date: 2022/9/7 - 15:14
