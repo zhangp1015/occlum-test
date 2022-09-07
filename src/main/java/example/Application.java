b336d61b3f113b528d76ae0a71cce499f0930fe5
Zhang <645311501@qq.com>
2022-09-07 16:08:20 +0800

    init

---
+++ src/main/java/example/Application.java
@@ -0,0 +1,20 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title: Application
+ * @Package: example
+ * @Description:
+ * @Author: zhangp
+ * @Date: 2022/9/7 - 15:11
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/example/controller/HelloController.java
@@ -0,0 +1,24 @@
+package example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title: HelloController
+ * @Package: example.controller
+ * @Description:
+ * @Author: zhangp
+ * @Date: 2022/9/7 - 15:14
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello World !";
+    }
+
+}
